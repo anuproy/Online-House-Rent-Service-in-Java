df642ca780c04846f31aa822cb466a61f563221a
unknown <anuproycode@gmail.com>
2020-04-03 17:22:35 +0600

    first commit

---
+++ Project/src/project/Project.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author SHUVO_PC
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         NewJFrame2 n1 = new NewJFrame2();
+        NewJFrame4 n2 = new NewJFrame4();
+        NewJFrame6 n3 = new NewJFrame6();
+        
+       
+        n1.setVisible(true);
+        n1.setVisible(true);
+        
+    }
+    
+}
